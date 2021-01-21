97f7e0b7bbc2b9ca27957b65d3b380745bac3df5
Marcelo Ferreira Cândido <fcandido.marcelo@gmail.com>
2021-01-21 14:01:47 -0300

    🚧 creating functions for: - compare a path with all others - verify if objective was achieved (without expressive testing yet) - choose next node in three to be open and beggining function to expand chosen node

--- A*/a_star/a_star.py
+++ A*/a_star/a_star.py
@@ -9,10 +9,52 @@ class A_Star(object):
         self.real_distances = np.genfromtxt(
             real_distances_filename, delimiter=",")
 
-        self.paths = [[{"id": self.objective[0],
-                        "straight_line_distance": self.heuristics[self.objective[0], self.objective[1]]}]]
+        self.paths = [[self.create_node(
+            self.objective[0], 0, self.heuristics[self.objective[0], self.objective[1]])]]
+
+    def create_node(self, id, current_path_length, heuristics_value):
+        return {"id": id,
+                "current_path_length": current_path_length,
+                "heuristics_value": heuristics_value,
+                "visited": False}
+
+    def compare_paths(self, path):
+        for path_for_comparison in self.paths:
+            if path[-1]["id"] == path_for_comparison[-1]["id"]:
+                continue
+            else:
+                if path[-1]["current_path_length"] + path[-1]["heuristics_value"] > path_for_comparison[-1]["current_path_length"] + path_for_comparison[-1]["heuristics_value"]:
+                    return False
+        return True
+
+    def verify_achieved_objective(self):
+        for path in self.paths:
+            if path[-1]["id"] == self.objective[1]:
+                if self.compare_paths(path[-1]):
+                    return True
+                else:
+                    return False
+        return False
+
+    def choose_next(self):
+        next = 0
+        smallest = self.paths[next][-1]["current_path_length"] + \
+            self.paths[next][-1]["heuristics_value"]
+
+        for i, path in enumerate(self.paths):
+            if path[-1]["current_path_length"] + path[-1]["heuristics_value"] < smallest:
+                smallest = path[-1]["current_path_length"] + \
+                    path[-1]["heuristics_value"]
+                next = i
+
+        return next
+
+    def expand_chosen(self, index):
+        id = self.paths[index][-1]["id"]
+
+        possibilities = np.nonzero(self.real_distances[id, :])[0]
+        print(id, possibilities)
 
     def run(self):
-        print(self.heuristics)
-        print(self.real_distances)
-        print(self.paths)
+        while not self.verify_achieved_objective():
+            self.expand_chosen(self.choose_next())
