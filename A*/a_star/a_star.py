c8229af3e28430ce695396d31c69857bf7838db0
Marcelo Ferreira Cândido <fcandido.marcelo@gmail.com>
2021-01-21 12:17:18 -0300

    🚧 creating arrays from data and beginning paths array

--- A*/a_star/a_star.py
+++ A*/a_star/a_star.py
@@ -4,8 +4,15 @@ import numpy as np
 class A_Star(object):
     def __init__(self, objective, heuristics_filename, real_distances_filename):
         self.objective = objective
-        self.heuristics = heuristics
-        self.real_distances = real_distances
+        self.heuristics = np.genfromtxt(
+            heuristics_filename, delimiter=",")
+        self.real_distances = np.genfromtxt(
+            real_distances_filename, delimiter=",")
+
+        self.paths = [[{"id": self.objective[0],
+                        "straight_line_distance": self.heuristics[self.objective[0], self.objective[1]]}]]
 
     def run(self):
-        pass
+        print(self.heuristics)
+        print(self.real_distances)
+        print(self.paths)
