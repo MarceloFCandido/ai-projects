7efe89fe2de5aab1a4c441dac145c0ebd5d0473e
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2021-03-06 21:45:41 -0300

    🧐 building algorithm, but its wrong in unknown way

--- perceptron/perceptron/perceptron.py
+++ perceptron/perceptron/perceptron.py
@@ -5,34 +5,51 @@ class IrisPerceptron(object):
     def __init__(self) -> None:
         super().__init__()
 
-        self.w = None
-        self.e = None
+        self.n_classes = 3
+        self.weigths = None
+        self.bias = None
 
     def train(self, max_it, learning_rate, X, d, activation_func) -> None:
-        N = X.shape[0]
+        n_lines, n_features = X.shape
 
-        b = 0.
-        self.w = np.zeros(N)
-
-        y = np.zeros(N)
-        self.e = np.zeros(N)
+        self.weigths = np.zeros((self.n_classes, n_features))
+        self.bias = np.zeros((self.n_classes, 1))
 
         epoch = 0
         err = 1.
 
         while epoch < max_it and err > 0.:
-            print(f'Training(Epoch: {epoch} | error: {err} | bias: {b})')
+            print(
+                f'Training(epoch: {epoch} | error: {err} | bias: {self.bias.T})')
             err = 0.
 
-            for i in range(N):
-                y[i] = activation_func(self.w[i] * X[i] + b)
-                self.e[i] = d[i] - y[i]
-                self.w[i] = self.w[i] + learning_rate * self.e[i] * X[i]
+            for i in range(n_lines):
+                y = activation_func(
+                    np.matmul(self.weigths, X[i].reshape((n_features, 1))) + self.bias)
 
-                b = b + learning_rate * self.e[i]
-                err = err + self.e[i] * self.e[i]
+                e = d[i].reshape((self.n_classes, 1)) - y
+
+                self.weigths = self.weigths + \
+                    learning_rate * \
+                    np.matmul(e, X[i].reshape((n_features, 1)).T)
+                self.bias = self.bias + learning_rate * e
+
+                err = err + np.sum(np.power(e, 2))
 
             epoch = epoch + 1
 
-    def test() -> None:
-        pass
+        print(self.weigths)
+
+    def test(self, X, d, activation_func) -> None:
+        n_lines, n_features = X.shape
+
+        correct = 0
+        for i in range(n_lines):
+            y = activation_func(
+                np.matmul(self.weigths, X[i].reshape((n_features, 1))) + self.bias)
+            e = d[i].reshape((self.n_classes, 1)) - y
+
+            if np.nonzero(e)[0].size == 0:
+                correct = correct + 1
+
+        return correct, correct * n_lines / 100
